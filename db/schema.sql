9aae02585997d89695c384917dd5533014c7a814
unknown <danleviobello@gmail.com>
2023-08-23 23:12:41 -0500

    Added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY Key,
+    title VARCHAR(100) NOT NULL,
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_id INT NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (job_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+);
